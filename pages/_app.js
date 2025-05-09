7beea6a42990158165dd2ec6a6ee7fc6ee2ec5e7
xbornid <36538144+xbornid@users.noreply.github.com>
2025-05-09 10:52:39 +0800

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,12 @@
 // pages/_app.js
 import '../styles/globals.css';
 import AuthProvider from '../context/AuthContext';
-// 1) import MiniKitProvider
-import { MiniKitProvider } from '@coinbase/onchainkit/minikit';
 
 function MyApp({ Component, pageProps }) {
   return (
-    // 2) wrap aplikasi dengan MiniKitProvider
-    <MiniKitProvider
-      apiKey={process.env.NEXT_PUBLIC_ONCHAINKIT_API_KEY}
-      chain="base"               // kita deploy di Base chain
-    >
-      <AuthProvider>
-        <Component {...pageProps} />
-      </AuthProvider>
-    </MiniKitProvider>
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
   );
 }
 
