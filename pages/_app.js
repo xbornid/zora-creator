8a5353c5a3718c2e846c169c46ab3fcdec6b25f4
xbornid <36538144+xbornid@users.noreply.github.com>
2025-05-13 15:14:39 +0800

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,26 +1,12 @@
 // pages/_app.js
 import '../styles/globals.css'
 import AuthProvider from '../context/AuthContext'
-// import MiniKitProvider dari OnchainKit
-import { MiniKitProvider } from '@coinbase/onchainkit/minikit'
-import { base } from 'viem/chains'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <MiniKitProvider
-      apiKey={process.env.NEXT_PUBLIC_ONCHAINKIT_API_KEY}
-      chain={base}
-      config={{
-        appearance: {
-          mode: 'auto',
-          theme: 'light',
-        },
-      }}
-    >
-      <AuthProvider>
-        <Component {...pageProps} />
-      </AuthProvider>
-    </MiniKitProvider>
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
   )
 }
 
